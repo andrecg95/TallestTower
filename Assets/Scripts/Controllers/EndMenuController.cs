a69a18215c7f7b4cd8cd9457ee1f7ca7160ac933
andrecg95 <andrecg95@gmail.com>
2023-08-02 22:02:54 +0100

    add sounds

--- Assets/Scripts/Controllers/EndMenuController.cs
+++ Assets/Scripts/Controllers/EndMenuController.cs
@@ -4,21 +4,28 @@ namespace TallestTower.Controllers
 {
     public class EndMenuController : MonoBehaviour
     {
-        [SerializeField] GameObject m_WinTitle, m_LoseTitle;
+        [SerializeField] GameObject m_Win, m_Lose;
+        [SerializeField] AudioSource m_GameMusic;
 
 
         public void ShowWin()
         {
             gameObject.SetActive(true);
-            m_WinTitle.SetActive(true);
-            m_LoseTitle.SetActive(false);
+            m_Win.SetActive(true);
+            m_Lose.SetActive(false);
+
+            m_Win.GetComponent<AudioSource>().Play();
+            m_GameMusic.Stop();
         }
 
         public void ShowLose()
         {
             gameObject.SetActive(true);
-            m_WinTitle.SetActive(false);
-            m_LoseTitle.SetActive(true);
+            m_Win.SetActive(false);
+            m_Lose.SetActive(true);
+
+            m_Lose.GetComponent<AudioSource>().Play();
+            m_GameMusic.Stop();
         }
     }
 }
